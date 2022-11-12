67ea81844d3eed20d3e9d47fd1d111a07942fa28
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-11-12 20:29:18 +0700

    feat: Structural Typing and Excess Property Checks

--- example-two.ts
+++ example-two.ts
@@ -27,3 +27,58 @@ function selectDeliveryAddress(addressOrIndex: unknown): string {
 // myFavouriteAddress is now string, even though we just
 // return true? This is going to blow up in runtime!
 // const myFavouriteAddress = selectDeliveryAddress(true)
+
+type Article = {
+  title: string;
+  price: number;
+  vat: number;
+  stock: number;
+  description: string;
+}
+
+const book = {
+  title: 'Form Design Patterns by Adam Silver',
+  price: 32.77,
+  vat: 0.19,
+  stock: 1000,
+  description: 'A practical book on accessibility and forms',
+}
+
+const movie: Article = {
+  title: 'Helvetica',
+  price: 6.66,
+  vat: 0.19,
+  stock: 1000,
+  description: '90 minutes of gushing about Helvetica',
+}
+
+const movBackup = {
+  title: 'Helvetica',
+  price: 6.66,
+  vat: 0.19,
+  stock: 1000,
+  description: '90 minutes of gushing about Helvetica',
+  rating: 5
+}
+
+const newMovie: Article = movBackup
+
+type ShopItem = {
+  title: string,
+  price: number,
+  vat: number,
+  stock: number,
+  description: string,
+  rating: number
+}
+
+const shopitem = {
+  title: 'Helvetica',
+  price: 6.66,
+  vat: 0.19,
+  stock: 1000,
+  description: '90 minutes of gushing about Helvetica',
+  rating: 5
+}
+
+const anotherMovie: Article = shopitem
