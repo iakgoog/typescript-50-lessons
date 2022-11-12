db8a6463c02c24527db60cae82cf519b0847ad57
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-11-12 21:13:02 +0700

    feat: typeof

--- example-two.ts
+++ example-two.ts
@@ -95,3 +95,66 @@ function createArticleElement(
 createArticleElement(shopitem)
 
 createArticleElement(anotherMovie)
+
+/**
+ * typeof
+ */
+
+type ArticleStub = {
+  price: number,
+  vat: number,
+  title: number
+}
+
+type Address = {
+  city: string,
+  zip: string,
+  street: string,
+  number: string,
+}
+
+type Customer = {
+  name: string,
+  addres: Address,
+  dateOfBirth: Date
+}
+
+// type Order = {
+//   articles: ArticleStub[],
+//   customer: Customer
+// }
+
+const defaultOrder = {
+  articles: [
+    {
+      price: 1200.50,
+      vat: 0.2,
+      title: 'Macbook Air Refurbished - 2013'
+    },
+    {
+      price: 9,
+      vat: 0,
+      title: 'I feel smashing subscription'
+    }
+  ],
+  customer: {
+    name: 'Fritz Furball',
+    address: {
+      city: 'Smashing Hall',
+      zip: '90210',
+      street: 'Whisker-ia Lane',
+      number: '1337'
+    },
+    dateOfBirth: new Date(2006, 9, 1)
+  }
+}
+
+type Order = typeof defaultOrder
+
+function checkOrders(orders: Order[]) {
+  let valid = true;
+  for (let order of orders) {
+    valid = valid && order.articles.length > 0
+  }
+  return valid  
+}
