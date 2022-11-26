fb854612ab619e78730c2c42e3c30ccfc9520e4a
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-11-26 21:23:59 +0700

    feat: Mapped and Indexed Access Types

--- example-five.ts
+++ example-five.ts
@@ -3,37 +3,37 @@
  */
 
 type VideoFormatURLs = {
-  format360p: URL,
-  format480p: URL,
-  format720p: URL,
-  format1080p: URL,
-}
+  format360p: URL;
+  format480p: URL;
+  format720p: URL;
+  format1080p: URL;
+};
 
-declare const videos: VideoFormatURLs
-declare function loadFormat(format: string): void
+declare const videos: VideoFormatURLs;
+declare function loadFormat(format: string): void;
 
 function isFormatAvailable(
   obj: VideoFormatURLs,
-  key: string,
+  key: string
 ): key is keyof VideoFormatURLs {
-  return key in obj
+  return key in obj;
 }
 
 if (isFormatAvailable<VideoFormatURLs>(videos, format)) {
-  videos[format]
+  videos[format];
 }
 
 type SubtitleURLs = {
-  english: URL,
-  german: URL,
-  french: URL,
-}
+  english: URL;
+  german: URL;
+  french: URL;
+};
 
 function isSubtitleAvailable(
   obj: SubtitleURLs,
-  key: string,
+  key: string
 ): key is keyof SubtitleURLs {
-  return key in obj
+  return key in obj;
 }
 
 /**
@@ -44,23 +44,21 @@ function isAvailable<Formats extends object>(
   obj: Formats,
   key: string | number | symbol
 ): key is keyof Formats {
-  return key in obj
+  return key in obj;
 }
 
 /**
  * Generic Annotations and Generic Inference
  */
 
-declare const videoFormats: VideoFormatURLs
+declare const videoFormats: VideoFormatURLs;
 
 if (isAvailable(videoFormats, format)) {
-
 }
 
-declare const subtitles: SubtitleURLs
+declare const subtitles: SubtitleURLs;
 
 if (isAvailable(subtitles, language)) {
-
 }
 
 /**
@@ -68,29 +66,26 @@ if (isAvailable(subtitles, language)) {
  */
 
 async function randomNumber() {
-  return Math.random()
+  return Math.random();
 }
 
-let anArray: number[]
+let anArray: number[];
 
-let anotherArray: Array<number>
+let anotherArray: Array<number>;
 
-let aMixedArray: Array<number | string | boolean>
+let aMixedArray: Array<number | string | boolean>;
 
 /**
  * Generic Constraints
  */
 
 if (isAvailable({ name: 'Stefan', age: 38 }, key)) {
-
 }
 
 if (isAvailable('A string', 'length')) {
-
 }
 
 if (isAvailable(1337, aKey)) {
-
 }
 
 /**
@@ -101,23 +96,22 @@ if (isAvailable(1337, aKey)) {
  * Index Types
  */
 
-type PossibleKeys = 'meetup' | 'conference' | 'hackathon' | 'webinar'
+type PossibleKeys = 'meetup' | 'conference' | 'hackathon' | 'webinar';
 
 type Groups = {
-  [k in PossibleKeys]: any
-}
+  [k in PossibleKeys]: any;
+};
 
 type AnyObject = {
-  [k: string]: any
-}
+  [k: string]: any;
+};
 
 type URLList = {
-  [k: string]: URL
-}
-
+  [k: string]: URL;
+};
 
 // function loadFile<Formats extends URLList>(
-//   fileFormats: Formats, 
+//   fileFormats: Formats,
 //   format: string
 // ) {
 
@@ -127,9 +121,9 @@ type URLList = {
  * Working with Keys
  */
 
-loadFile(videos, 'format1080p')
+loadFile(videos, 'format1080p');
 
-loadFile(videos, 'format4k')
+loadFile(videos, 'format4k');
 // 4K not available
 // TypeScript doesn't squiggle
 
@@ -140,18 +134,16 @@ loadFile(videos, 'format4k')
 function loadVideoFormat(
   fileFormats: VideoFormatURLs,
   format: keyof VideoFormatURLs
-) {
-
-}
+) {}
 
 type URLObject = {
-  [k: string]: URL
-}
+  [k: string]: URL;
+};
 
 type Loaded<Key> = {
-  format: Key,
-  loaded: boolean
-}
+  format: Key;
+  loaded: boolean;
+};
 
 // function loadFile<Formats extends URLObject>(
 //   fileFormats: Formats,
@@ -160,27 +152,24 @@ type Loaded<Key> = {
 
 // }
 
-async function loadFile<
-  Formats extends URLObject,
-  Key extends keyof Formats
->(
+async function loadFile<Formats extends URLObject, Key extends keyof Formats>(
   fileFormats: Formats,
   format: Key
 ): Promise<Loaded<Key>> {
-  const data = await fetch(fileFormats[format].href)
+  const data = await fetch(fileFormats[format].href);
   return {
     format,
-    loaded: data.status === 200
-  }
+    loaded: data.status === 200,
+  };
 }
 
-(async() => {
-  const result = await loadFile(videos, 'format1080p')
+(async () => {
+  const result = await loadFile(videos, 'format1080p');
 
   if (result.format !== 'format1080p') {
-    throw new Error('Your implementation is wrong')
+    throw new Error('Your implementation is wrong');
   }
-})()
+})();
 
 /**
  * Generic Mapped Types
@@ -190,7 +179,7 @@ async function loadFile<
  * Pick
  */
 
-type HD = Pick<VideoFormatURLs, 'format1080p' | 'format720p'>
+type HD = Pick<VideoFormatURLs, 'format1080p' | 'format720p'>;
 
 // type HD = {
 //   format1080: URL,
@@ -202,3 +191,78 @@ type HD = Pick<VideoFormatURLs, 'format1080p' | 'format720p'>
  */
 
 // type URLObject = Record<string, URL>
+
+/**
+ * Mapped and Indexed Access Types
+ */
+
+type Format360 = {
+  format360p: URL;
+};
+
+type Format480 = {
+  format480p: URL;
+};
+
+type Format720 = {
+  format720p: URL;
+};
+
+type Format1080 = {
+  format1080p: URL;
+};
+
+// type AvailableFormats = Format360 | Format480 | Format720 | Format1080;
+
+const hq: AvailableFormats = {
+  format720p: new URL('...'),
+  format1080p: new URL('...'),
+}
+
+const lofi: AvailableFormats = {
+  format360p: new URL('...'),
+  format480p: new URL('...'),
+}
+
+// type Split = keyof VideoFormatURLs
+// Equivalent to
+// type Split = 'format360p' | 'format480p' | 'format720p' | 'format1080p'
+
+// type Split = {
+//   [P in keyof VideoFormatURLs]: P
+// }
+// Equivalent to
+// type Split = {
+//   format360p: 'format360p',
+//   format480p: 'format480p',
+//   format720p: 'format720p',
+//   format1080p: 'format1080'
+// }
+
+// type Split = {
+//   [P in keyof VideoFormatURLs]: P
+// }[keyof VideoFormatURLs]
+// Equivalent to
+// type Split = 'format360p' | 'format480p' | 'format720p' | 'format1080p'
+
+// type Split = {
+//   [P in keyof VideoFormatURLs]: Record<P, VideoFormatURLs[P]>
+// }[keyof VideoFormatURLs]
+// Equivalent to
+// type Split =
+//   Record<'format360p', URL> |
+//   Record<'format480p', URL> |
+//   Record<'format720p', URL> |
+//   Record<'format1080p', URL>
+// Equivalent to
+// type Split =
+//   { format360p: URL } |
+//   { format480p: URL } |
+//   { format720p: URL } |
+//   { format1080p: URL }
+
+type Split<Obj> = {
+  [Prop in keyof Obj]: Record<Prop, Obj[Prop]>
+}[keyof Obj]
+
+type AvailableFormats = Split<VideoFormatURLs>
