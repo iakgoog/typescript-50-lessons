e456ae379a414cb894f22898fe7b29132d1849ea
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-11-28 05:40:00 +0700

    feat: Function Overloads Are Fine

--- example-six.ts
+++ example-six.ts
@@ -7,57 +7,59 @@
  */
 
 type Customer = {
-  customerId: number,
-  firstName: string,
-  lastName: string
-}
+  customerId: number;
+  firstName: string;
+  lastName: string;
+};
 
 const customer = {
   customerId: 1,
   firstName: 'Stefan',
-  lastName: 'Baumgartner'
-}
+  lastName: 'Baumgartner',
+};
 
 type Product = {
-  productId: number,
-  title: string,
-  price: number
-}
+  productId: number;
+  title: string;
+  price: number;
+};
 
 const product = {
   productId: 22,
   title: 'Form Design Patterns',
-  price: 29
-}
+  price: 29,
+};
 
 type Order = {
-  orderId: number,
-  customer: Customer,
-  products: Product[],
-  date: Date
-}
+  orderId: number;
+  customer: Customer;
+  products: Product[];
+  date: Date;
+};
 
-function fetchOrder(customer: Customer): Order[]
-function fetchOrder(product: Product): Order[]
-function fetchOrder(orderId: number): Order
-function fetchOrder(param: Customer | Product): Order[]
-function fetchOrder(param: Customer | number): Order[] | Order
-function fetchOrder(param: Product | number): Order[] | Order
-function fetchOrder(param: Customer | Product | number): Order[] | Order
-function fetchOrder<Param extends FetchParams>(param: Param): FetchReturn<Param> {
-}
+// function fetchOrder(customer: Customer): Order[];
+// function fetchOrder(product: Product): Order[];
+// function fetchOrder(orderId: number): Order;
+// function fetchOrder(param: Customer | Product): Order[];
+// function fetchOrder(param: Customer | number): Order[] | Order;
+// function fetchOrder(param: Product | number): Order[] | Order;
+// function fetchOrder(param: Customer | Product | number): Order[] | Order;
+// function fetchOrder<Param extends FetchParams>(
+//   param: Param
+// ): FetchReturn<Param>
 
-fetchOrder(customer)
-fetchOrder(product)
-fetchOrder(2)
+// fetchOrder(customer);
+// fetchOrder(product);
+// fetchOrder(2);
 
-declare const ambigous: Customer | number
+// declare const ambigous: Customer | number;
 
-fetchOrder(ambigous) // It's any
+// fetchOrder(ambigous); // It's any
 
-declare x: any
+// declare;
+// x: any;
 
-fetchOrder(x)
+// fetchOrder(x);
 
 /**
  * Enter Conditional Types
@@ -65,57 +67,90 @@ fetchOrder(x)
 
 // type Conditional<T> = T extends U ? A : B
 
-type FetchParams = number
-  | Customer
-  | Product;
+type FetchParams = number | Customer | Product;
 
-type FetchReturn<Param extends FetchParams> =
-  Param extends Customer ? Order[] :
-  Param extends Product ? Order[] : Order
+type FetchReturn<Param extends FetchParams> = Param extends Customer
+  ? Order[]
+  : Param extends Product
+    ? Order[]
+    : Order;
 
 /**
  * Combining Function Overloads and Conditional Types
  */
 
-type Callback<Res> = (result: Res) => void
+type Callback<Res> = (result: Res) => void;
 
 function fetchOrder<Par extends FetchParams>(
   inp: Par
-): Promise<FetchReturn<Par>>
+): Promise<FetchReturn<Par>>;
 
 function fetchOrder<Par extends FetchParams>(
-  inp: Par, fun: Callback<FetchReturn<Par>>
-): void
+  inp: Par,
+  fun: Callback<FetchReturn<Par>>
+): void;
 
 /**
  * Tuple Types for Function Heads
  */
 
 function doSomething(...rest) {
-  return rest[0] + rest[1]
+  return rest[0] + rest[1];
 }
 
-doSomething('Java', 'Script')
+doSomething('Java', 'Script');
 
 function fetchOrder<Par extends FetchParams>(
   ...args: [Par, Callback<FetchReturn<Par>>]
-): void
+): void;
 
 function fetchOrder<Par extends FetchParams>(
   ...args: [Par]
-): Promise<FetchReturn<Par>>
+): Promise<FetchReturn<Par>>;
 
-type FetchCb<T extends FetchParams> = Callback<FetchReturn<T>>
+type FetchCb<T extends FetchParams> = Callback<FetchReturn<T>>;
 
-type AsyncResult<
-  FHead, Par extends FetchParams
-> = FHead extends [Par, FetchCb<Par>]
+type AsyncResult<FHead, Par extends FetchParams> = FHead extends [
+  Par,
+  FetchCb<Par>
+]
     ? void
     : FHead extends [Par]
       ? Promise<FetchReturn<Par>>
       : never;
 
-function fetchOrder<
-  Par extends FetchParams,
-  FHead
->(...args: FHead): AsyncResult<FileSystemHandle, Par>
+function fetchOrder<Par extends FetchParams, FHead>(
+  ...args: FHead
+): AsyncResult<FileSystemHandle, Par>;
+
+/**
+ * Function Overloads Are Fine
+ */
+
+// Version 1
+function fetchOrder<Par extends FetchParams>(
+  inp: Par
+): Promise<FetchReturn<Par>>;
+
+// Version 2
+function fetchOrder<Par extends FetchParams>(
+  inp: Par,
+  fun: Callback<FetchReturn<Par>>
+): void;
+
+// The implementation
+function fetchOrder<Par extends FetchParams>(
+  inp: Par,
+  fun?: Callback<FetchReturn<Par>>
+): Promise<FetchReturn<Par>> | void {
+  const res = fetch(`/backend?inp=${JSON.stringify(inp)}`)
+                .then(res => res.json())
+
+  if (fun) {
+    res.then(result => {
+      fun(result)
+    })
+  } else {
+    return res
+  }
+}
