6837be8ec12fadd94c2f0f050f129f038beb452a
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-12-16 06:12:23 +0700

    feat: Custom defineProperty

--- example-seven.tsx
+++ example-seven.tsx
@@ -526,3 +526,57 @@ function assertIsNum(val: any): asserts val is number {
     throw new AssertionError('Not a number!');
   }
 }
+
+/**
+ * Custom defineProperty
+ */
+
+function defineProperty<
+  Obj extends object,
+  Key extends PropertyKey,
+  PDesc extends PropertyDescriptor
+>(obj: Obj, prop: Key, val: PDesc): asserts obj is Obj & DefineProperty<Key, PDesc> {
+  Object.defineProperty(obj, prop, val);
+}
+
+type DefineProperty<
+  Prop extends PropertyKey,
+  Desc extends PropertyDescriptor
+> = Desc extends { 
+  writable: any,
+  set(val: any): any
+}
+  ? never
+  : Desc extends {
+    writable: any,
+    get(): any
+  }
+    ? never
+    : Desc extends {
+      writable: false
+    }
+      ? Readonly<InferValue<Prop, Desc>>
+      : Desc extends {
+        writable: true
+      }
+        ? InferValue<Prop, Desc>
+        : Readonly<InferValue<Prop, Desc>>
+
+type InferValue<Prop extends PropertyKey, Desc> =
+  Desc extends {
+    get(): any,
+    value: any
+  }
+    ? never
+    : Desc extends {
+      value: infer T
+    }
+      ? Record<Prop, T>
+      : Desc extends {
+        get(): infer T
+      }
+        ? Record<Prop, T>
+        : never;
+
+
+
